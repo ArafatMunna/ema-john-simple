38d1f9aeec6d92e6a991f4b8a819efb8c8c84ef2
ArafatMunna <arafatmunna000@gmail.com>
2022-03-31 01:10:32 +0600

    router and order review added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Cart.css";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, children }) => {
     // console.log(cart);
     let total = 0;
     let shipping = 0;
@@ -39,6 +39,7 @@ const Cart = ({ cart }) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            {children}
         </div>
     );
 };
