8c7d046791ba0c0602b01a79aa84191cee480754
ArafatMunna <arafatmunna000@gmail.com>
2022-04-26 02:11:35 +0600

    heroku server added

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -9,7 +9,7 @@ const useCart = () => {
         const savedCart = [];
         const keys = Object.keys(storedCart);
         // console.log(keys);
-        fetch("http://localhost:5000/productByKeys", {
+        fetch("https://ema-john-server-95.herokuapp.com/productByKeys", {
             method: "POST",
             headers: {
                 "content-type": "application/json",
